3cfe403598cbd73b0efb99d77921b8dbdafbcf45
Andrey Stankevich <stankevich.as@phystech.edu>
2025-01-14 01:16:47 +0300

    altered method signatures of network class, removed adm checkpoint dependency

--- i2sb/network.py
+++ i2sb/network.py
@@ -19,32 +19,25 @@ from .ckpt_util import (
     I2SB_IMG256_COND_CKPT,
 )
 
-from ipdb import set_trace as debug
-
 class Image256Net(torch.nn.Module):
-    def __init__(self, log, noise_levels, use_fp16=False, cond=False, pretrained_adm=True, ckpt_dir="data/"):
+    
+    def __init__(self, log, opt, noise_levels):
+        
         super(Image256Net, self).__init__()
 
-        # initialize model
-        ckpt_pkl = os.path.join(ckpt_dir, I2SB_IMG256_COND_PKL if cond else I2SB_IMG256_UNCOND_PKL)
+        # checkpoint with options is created prior to the net instance        
+        ckpt_pkl = os.path.join(opt.ckpt_path, 'options.pkl')
         with open(ckpt_pkl, "rb") as f:
             kwargs = pickle.load(f)
-        kwargs["use_fp16"] = use_fp16
+    
         self.diffusion_model = create_model(**kwargs)
         log.info(f"[Net] Initialized network from {ckpt_pkl=}! Size={util.count_parameters(self.diffusion_model)}!")
 
-        # load (modified) adm ckpt
-        if pretrained_adm:
-            ckpt_pt = os.path.join(ckpt_dir, I2SB_IMG256_COND_CKPT if cond else I2SB_IMG256_UNCOND_CKPT)
-            out = torch.load(ckpt_pt, map_location="cpu")
-            self.diffusion_model.load_state_dict(out)
-            log.info(f"[Net] Loaded pretrained adm {ckpt_pt=}!")
-
         self.diffusion_model.eval()
-        self.cond = cond
+        self.cond = opt.cond_y
         self.noise_levels = noise_levels
 
-    def forward(self, x, steps, cond=None):
+    def forward(self, x, steps, cond):
 
         t = self.noise_levels[steps].detach()
         assert t.dim()==1 and t.shape[0] == x.shape[0]
