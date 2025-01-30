993576fcec037b7490963d96dc6138964d92093b
Andrey Stankevich <stankevich.as@phystech.edu>
2025-01-30 22:32:38 +0300

    emloyed config added in 98533e6 to initialize network

--- i2sb/network.py
+++ i2sb/network.py
@@ -5,36 +5,28 @@
 # for I2SB. To view a copy of this license, see the LICENSE file.
 # ---------------------------------------------------------------
 
-import os
-import pickle
 import torch
 
-from guided_diffusion.script_util import create_model
+from .import util
+from guided_diffusion.unet import UNetModel
+from config import get_config_by_name
 
-from . import util
-from .ckpt_util import (
-    I2SB_IMG256_UNCOND_PKL,
-    I2SB_IMG256_UNCOND_CKPT,
-    I2SB_IMG256_COND_PKL,
-    I2SB_IMG256_COND_CKPT,
-)
 
 class Image256Net(torch.nn.Module):
-    
+
     def __init__(self, log, opt, noise_levels):
         
         super(Image256Net, self).__init__()
 
-        # checkpoint with options is created prior to the net instance        
-        ckpt_pkl = os.path.join(opt.ckpt_path, 'options.pkl')
-        with open(ckpt_pkl, "rb") as f:
-            kwargs = pickle.load(f)
+        model_config = get_config_by_name(opt.model)
+        model_config.image_size = opt.image_size
     
-        self.diffusion_model = create_model(**kwargs)
-        log.info(f"[Net] Initialized network from {ckpt_pkl=}! Size={util.count_parameters(self.diffusion_model)}!")
+        self.diffusion_model = UNetModel(**model_config)
+
+        log.info(f"[Net] Created network with {opt.model} architecture! Size={util.count_parameters(self.diffusion_model)}!")
 
         self.diffusion_model.eval()
-        self.cond = opt.cond_y
+        
         self.noise_levels = noise_levels
 
     def forward(self, x, steps, cond):
@@ -42,5 +34,5 @@ class Image256Net(torch.nn.Module):
         t = self.noise_levels[steps].detach()
         assert t.dim()==1 and t.shape[0] == x.shape[0]
 
-        x = torch.cat([x, cond], dim=1) if self.cond else x
+        x = torch.cat([x, cond], dim=1)
         return self.diffusion_model(x, t)
